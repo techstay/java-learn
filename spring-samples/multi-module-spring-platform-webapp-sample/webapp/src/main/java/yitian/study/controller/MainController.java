56093741d77fb2a13645c83eb8f9363834e018e7
yitian <lovery521@gmail.com>
2018-09-23 19:06:41 +0800

    把基于Spring IO Platform的多模块Gradle项目示例的主页用element-ui弄得好看了一点（虽然没卵用）

--- spring-samples/multi-module-spring-platform-webapp-sample/webapp/src/main/java/yitian/study/controller/MainController.java
+++ spring-samples/multi-module-spring-platform-webapp-sample/webapp/src/main/java/yitian/study/controller/MainController.java
@@ -3,8 +3,12 @@ package yitian.study.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import yitian.study.booklist.IBookList;
+
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -12,8 +16,14 @@ public class MainController {
     private IBookList myBookList;
 
     @RequestMapping("/")
-    String index(Model model) {
-        model.addAttribute("booklist",myBookList.getBookList());
+    String index() {
         return "index";
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/booklist", produces = "application/json")
+    @ResponseBody
+    List<String> bookList() {
+        return myBookList.getBookList();
     }
 }
